884ab59bace9dc9fcaca8fa0d479a17b8c247681
vijay <VijayChowdary94>
2024-10-24 10:44:56 -0400

    modified by Vijay

--- src/test/java/com/resources/DummyClass.java
+++ src/test/java/com/resources/DummyClass.java
@@ -9,4 +9,11 @@ public class DummyClass {
 	void method2() {
 		System.out.println("I am method 2");
 	}
+	void method3() {
+		System.out.println("I am method 1");
+	}
+	
+	void method4() {
+		System.out.println("I am method 2");
+	}
 }
