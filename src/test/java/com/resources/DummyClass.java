ea8f6025fb2bd41eb096494992581908106a6558
vijay <VijayChowdary94>
2024-10-24 10:33:59 -0400

    Committed by Kumar

---
+++ src/test/java/com/resources/DummyClass.java
@@ -0,0 +1,12 @@
+package com.resources;
+
+public class DummyClass {
+
+	void method1() {
+		System.out.println("I am method 1");
+	}
+	
+	void method2() {
+		System.out.println("I am method 2");
+	}
+}
